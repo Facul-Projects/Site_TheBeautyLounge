9f41326c9608cd18934de627733f07f42e127930
Winicius Pires <143422028+Virus-Z@users.noreply.github.com>
2025-02-17 11:14:06 -0300

    Create processa_formulario.php

---
+++ php/processa_formulario.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $mensagem = $_POST["mensagem"];
+    
+    echo "<h1>Dados Enviados</h1>";
+    echo "<p><strong>Nome:</strong> $nome</p>";
+    echo "<p><strong>Email:</strong> $email</p>";
+    echo "<p><strong>Mensagem:</strong> $mensagem</p>";
+} else {
+    echo "Método de requisição inválido.";
+}
+?>
